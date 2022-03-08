eea76b2a6fb028c99d97c970d9a20665e2ac4caa
manodecristiano <cristian.carre.online@gmail.com>
2022-03-08 15:18:05 +0100

    call back HELL

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import "./styles.css";
 import { buscarHeroe } from "./js/callbacks";
 
 const heroeId1 = 'capi';
+const heroeId2 = 'spider';
+
+buscarHeroe( heroeId1, (err, heroe1) => {
 
 
-buscarHeroe( heroeId1, (err, heroe) => {
+   if ( err ){ console.error( err ); }
+    
+   buscarHeroe( heroeId2 , ( err, heroe2 )  => {
+  
+     if ( err ){ console.error( err ); }
 
+     console.log(`Enviando a ${ heroe1.nombre }y ${heroe2.nombre} a la misión`);
 
-if ( err ){
-    console.error( err );
-}else{
-    console.info( heroe );
-}
+   });
 
 
 })
