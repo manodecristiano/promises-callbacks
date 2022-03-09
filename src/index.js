f3377aa4ce18d9c9a88b0fd361d75fe8b6b4e30b
manodecristiano <cristian.carre.online@gmail.com>
2022-03-09 12:36:29 +0100

    promise.race

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+import {promesaLenta,promesaMedia,promesaRapida} from "./js/promises";
+
+
+/*promesaLenta.then( console.log );
+promesaMedia.then( console.log );
+promesaRapida.then( console.log );
+*/
+
+Promise.race([promesaLenta,promesaMedia,promesaRapida])
+.then( console.log )
+.catch( console.warn );
--- src/js/promises.js
+++ src/js/promises.js
@@ -26,9 +26,23 @@ export const buscarHeroe = (id ) => {
            reject(`No existe un héroe con el id ${ id }`);
        }
 
-
     })
-
-
-
 }
+
+
+    const promesaLenta = new Promise ( (resolve,reject) => {
+      setTimeout( () => {resolve('Promesa Lenta')},2000);
+
+    });
+
+    const promesaMedia = new Promise ( (resolve,reject) => {
+        setTimeout( () => {resolve('Promesa Media')},1500);
+  
+    });
+
+    const promesaRapida = new Promise ( (resolve,reject) => {
+        setTimeout( () => {resolve('Promesa Rápida')},1000);
+  
+    });
+
+export {promesaLenta, promesaMedia, promesaRapida};
