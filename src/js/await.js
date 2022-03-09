3cb5b0525eb969c1c1eee004d97fa886ec3e6780
manodecristiano <cristian.carre.online@gmail.com>
2022-03-09 19:09:02 +0100

    best tip to use await

--- src/js/await.js
+++ src/js/await.js
@@ -5,16 +5,23 @@ const heroesIds = ['capi', 'iron', 'spider'];
 
 export const obtenerHeroesArr = async () => {
    
-    const heroesArr = [];
+//1 2 3//  const heroesArr = [];
 
-    for (const id of heroesIds){
-    //buscarHeroeAsync(id).then(heroe => heroesArr.push( heroe ));
+//1 2 3// for (const id of heroesIds){
+   
+//1// buscarHeroeAsync(id).then(heroe => heroesArr.push( heroe )); 
+    
+//2// const heroe = await buscarHeroe( id );
+//2// heroesArr.push( heroe );
 
-    // const heroe = await buscarHeroe( id );
-    // heroesArr.push( heroe );
+//3//    heroesArr.push( buscarHeroe ( id ) );
 
-        heroesArr.push( buscarHeroe ( id ) );
+//1 2 3// }
 
-   }
-   return await Promise.all( heroesArr );
+//1// return  heroesArr;
+//2// return  await Promise.all( heroesArr) ;
+//3// return await Promise.all( heroesArr );
+
+   return await Promise.all( heroesIds.map( buscarHeroe ));
+
 }
